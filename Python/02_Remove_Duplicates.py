6dbd02ffb53895c744ae7362ed8bb97e14376811
Harshanaidu6 <harshavnaidu7@gmail.com>
2025-12-02 23:17:29 +0530

    Update remove_duplicates to process user input
    Refactor remove_duplicates function to handle user input and print results directly.

--- Python/02_Remove_Duplicates.py
+++ Python/02_Remove_Duplicates.py
@@ -1,8 +1,13 @@
-def remove_duplicates(text):
-    unique = ""
-    
-    for char in text:
-        if char not in unique:   
-            unique += char
+ # User input
+string = input("Enter a string: ")
 
-    return unique
+unique = ""  
+
+# Logic using simple loop
+for ch in string:
+    if ch not in unique:
+        unique += ch
+
+# Print result
+print("String after removing duplicates:", unique)
+
